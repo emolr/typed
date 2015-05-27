c450afa24c392d6b6d6686b480156951a910620b
Dennis Haulund Nielsen <dhni@nodes.dk>
2015-05-27 08:28:44 +0200

    Animation Exploration

--- app/modules/documents/overview/overview.animation.js
+++ app/modules/documents/overview/overview.animation.js
@@ -3,11 +3,19 @@
 
 	angular
 		.module('overview')
-		.animation('.documents__overview', Animation)
+		//.animation('.documents__overview-wrapper', Animation);
+
 
 	/* @ngInject */
-	function Animation() {
-		/*jshint validthis: true */
+	function Animation(TimelineMax) {
+		console.log('anm')
+		return {
+			enter: enter
+		};
+
+		function enter(element, doneFn) {
+			console.log('Haha, hej fra animation', element);
+		}
 	}
 
 })();
