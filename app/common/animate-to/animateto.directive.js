9c0056a06781d4e78957e75368ae475e1b664685
Emil Møller <emilrmoeller@gmail.com>
2015-05-30 17:58:30 +0200

    Changed animate to to be a simpler directive with no relation to what it's animating to.
    Also it now clones the DOM Node instead of creating a new from scratch
    to provide more control of the animation leave of the object being
    cloned.

--- app/common/animate-to/animateto.directive.js
+++ app/common/animate-to/animateto.directive.js
@@ -9,7 +9,10 @@
 	function animateTo($document) {
 		var directive = {
 			link: link,
-			restrict: 'EA'
+			restrict: 'EA',
+			scope: {
+				endFn: '&'
+			}
 		};
 
 		return directive;
@@ -17,44 +20,59 @@
 		function link(scope, element, attrs){
 
 			var fromEl = element[0];
-			var toEl = angular.element(attrs.animateTo)[0];
 
-			var elSize = fromEl.getBoundingClientRect();
-
-			console.log(toEl)
+			var elSize = element[0].getBoundingClientRect();
 
 			element.on('click', function(event) {
 
-				var tempEl = createNode(elSize);
-				TweenMax.set(toEl, {opacity: 0});
+				var tempEl = createAnchorNode(element, elSize);
+				var tempElChilds = tempEl.children;
 
+				// Append the cloned element to body
 				document.getElementsByTagName('body')[0].appendChild(tempEl);
 
-				TweenMax.to(tempEl, 5, {
+
+				// Animate the cloned element
+				TweenMax.to(tempEl, .6, {
 					top: 0,
 					left: 0,
 					width: '100%',
 					height: '100%',
-					ease: Power2.easeInOut
+					ease: Power2.easeInOut,
+					onComplete: function() {
+						scope.endFn();
+					}
 				});
+
+
+				// Animate all children independently
+				for (var i = 0, len = tempElChilds.length; i < len; i++ ) {
+					TweenMax.to(tempElChilds[i], .4, {
+						opacity: 0
+					});
+				}
 
 			});
 
 		}
 
-		function createNode(dimensions) {
+		function createAnchorNode(el, dimensions) {
+			var newAnchorEl = el[0].cloneNode(true);
 
-			var newEl = document.createElement('div');
-			newEl.className = 'animate-to__transporter';
-			newEl.style.position = 'absolute';
-			newEl.style.width = dimensions.width + 'px';
-			newEl.style.height = dimensions.height + 'px';
-			newEl.style.top = dimensions.top + 'px';
-			newEl.style.right = dimensions.right + 'px';
-			newEl.style.bottom = dimensions.bottom + 'px';
-			newEl.style.left = dimensions.left + 'px';
-			return newEl;
+			newAnchorEl.className += ' ' + 'animate-anchor-out';
+			newAnchorEl.style.position = 'absolute';
+			newAnchorEl.style.width = dimensions.width + 'px';
+			newAnchorEl.style.height = dimensions.height + 'px';
+			newAnchorEl.style.top = dimensions.top + 'px';
+			newAnchorEl.style.right = dimensions.right + 'px';
+			newAnchorEl.style.bottom = dimensions.bottom + 'px';
+			newAnchorEl.style.left = dimensions.left + 'px';
 
+			return newAnchorEl;
+		}
+
+		function fadeOutChild(element, index, array) {
+			this.hide();
 		}
 
 	}
